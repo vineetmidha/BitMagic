c6f15960c912138e9a4f046c419a64302bdd0506
Vineet Midha <vineet.midha@yahoo.com>
2020-10-27 11:52:25 +0530

    Create TurnOffBit.cpp

---
+++ TurnOffBit.cpp
@@ -0,0 +1,34 @@
+// Turn a bit OFF
+
+bool checkBit(int n, int bit)
+{
+    int mask = (1 << bit);
+    
+    return (n & mask) == 0;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int bit;
+    cin >> bit;
+
+    if (checkBit(n, bit))
+        cout << bit << " Not Set" << endl;
+    else
+        cout << bit << " Set" << endl;    
+
+    int mask = ~(1 << bit);
+    
+    n = (n & mask);
+    
+    if (checkBit(n, bit))
+        cout << bit << " Not Set";
+    else
+        cout << bit << " Set";    
+
+    return 0;
+}
+
