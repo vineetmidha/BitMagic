0fb31254f1eda110e5877e1fb6ef04278d109f26
Vineet Midha <vineet.midha@yahoo.com>
2020-10-31 14:39:38 +0530

    Create MaximizingXor.cpp

---
+++ MaximizingXor.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/maximizing-xor/problem
+
+int maximizingXor(int l, int r) {
+    int max = 0;
+    for (int i=l; i<=r; i++){
+        for (int j=i+1; j<=r; j++){
+            if ((i^j) > max){
+                max = (i^j);
+            }
+        }
+    }
+    return max;
+}
+
